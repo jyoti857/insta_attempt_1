2ea66f17b6f2d884aed7d30f85267338245c627e
Jyoti RS <jyotiranjans@cnxsi.com>
2020-02-18 10:39:33 +0530

    all ok till now, needs to improve the comment page

--- src/screens/comments/index.js
+++ src/screens/comments/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
-import {Button} from 'react-native-paper';
+import { View, Text, TextInput, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import {Button, Avatar} from 'react-native-paper';
 import CustomTextInput from '../../component/CustomTextInput';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { TouchableOpacity, ScrollView } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 // class Comments extends React.Component{
 
@@ -29,7 +30,10 @@ class Comments extends React.Component{
         this.fieldOne = React.createRef();
         this.state = {
             comment: '', 
-            commentsArr : []
+            commentsArr : [],
+            commentLike: false,
+            postTime : 0,
+            diff: 0,
         };
         // this.fieldOne.current.focus();
     }
@@ -39,30 +43,77 @@ class Comments extends React.Component{
     }
     handlePost = () => {
         // this.setState({commentsArr:this.state.commentsArr.concat(this.state.comment)});
-        this.setState({commentsArr:[...this.state.commentsArr, this.state.comment ], comment: ""});
-        console.log("from handle post -==-=->", this.state.commentsArr, "@@)*)$@", this.state.comment)
+        this.setState({
+                commentsArr:[...this.state.commentsArr, this.state.comment ], 
+                comment: "",
+                postTime: new Date().getSeconds(),
+
+            });
+        // console.log("from handle post -==-=->", this.state.commentsArr, "@@)*)$@", this.state.comment)
+    }
+    handleCommentLike = () =>{
+        this.setState({commentLike: !this.state.commentLike})
+    }
+    // handleTimePost = () => {
+    //     this.postTime = new Date().getTime();
+    //     console.log("from Comments page time ---> ", this.postTime)
+    // }
+    displayTimePost  = () => {
+        const currentTime = new Date().getSeconds();
+        const diff  = currentTime-this.state.postTime;
+        // if(diff < 1){
+        //      <Text>now</Text>
+        // }
+        // this.setState({diff});
+        return <Text>{diff}s</Text>
+    }
+    componentDidUpdate(){
+        setTimeout(() => this.displayTimePost(), 2000);
     }
     render(){
         return(
             <View>
-                {this.state.commentsArr.map(comment=>{
-                    return <Text style={{margin: 3, fontSize:20}}>{comment}</Text>
-                })}
-                <View style ={{marginTop: 300, flexDirection: "row", 
-                justifyContent: "space-between"}}>
+                <ScrollView contentContainerStyle = {{zIndex: -1}}>
+                    {this.state.commentsArr.map(comment=>{
+                        return (
+                            <View>
+                                <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: 'center'}}>
+                                <View style = {{flexDirection: 'row', alignItems: 'center'}}>
+                                    <Avatar.Image 
+                                    size = {40} source = {require('../../../assets/sofi1.jpg')}
+                                    style = {{margin:10}}
+                                    />
+                                    <Text style={{margin: 3, fontSize:20}}>{comment}</Text>
+                                </View>
+                                <Icon name = {this.state.commentLike ? 'heart': 'heart-o'} size={20}
+                                    onPress = {this.handleCommentLike}
+                                    style={{marginRight: 20}}
+                                />
+                                </View>
+                                {/* <Text>{setTimeout(this.displayTimePost(), 2000)}</Text> */}
+																<View style = {{flexDirection: 'row', paddingLeft: 20}}>
+																	<Text style = {{fontSize: 20}}>{this.displayTimePost()}</Text>
+																	<Text style = {{marginLeft: 40, fontSize: 20, color: "grey" }}>Reply</Text>
+																</View>
+                            </View>
+                        )
+                    })}
+                </ScrollView>
+                <KeyboardAvoidingView style ={{marginTop: 300, flexDirection: "row", 
+                justifyContent: "space-between", position: "absolute"}}>
                     <TextInput  
-                        placeholder = 'post your comment'
-                        autoCapitalize = 'words'
-                        autoCorrect = {false}
-                        autoFocus = {false}
-                        // keyboardType = 'default'
-                        value = {this.state.comment}
-                        ref = {this.fieldOne}
-                        style={{fontSize:30, marginLeft:10}}
-                        onChangeText = {text => this.setState({comment: text})}    
+											placeholder = 'post your comment'
+											// autoCapitalize = 'words'
+											autoCorrect = {false}
+											autoFocus = {false}
+											// keyboardType = 'default'
+											value = {this.state.comment}
+											ref = {this.fieldOne}
+											style={{fontSize:20, paddingLeft:10}}
+											onChangeText = {text => this.setState({comment: text})}    
                     />
-                    <Button onPress = {this.handlePost} style ={{position: "relative", marginRight: 20}}>Post</Button>
-                </View>
+                    <Button onPress = {this.handlePost} style ={{position: "relative", marginRight: 90}}>Post</Button>
+                </KeyboardAvoidingView>
                 {/* <TouchableOpacity onPress = {() => {
                     const textInput = this.fieldOne.current;
                     textInput.focus();
